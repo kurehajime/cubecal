77136be660c9397d7c669ef8311267f3017fbfa2
kurehajime <xiidec@gmail.com>
2026-04-16 22:38:28 +0900

    台座を小さく

--- src/components/stage/CalendarBase.tsx
+++ src/components/stage/CalendarBase.tsx
@@ -17,54 +17,54 @@ export function CalendarBase() {
       </RigidBody>
 
       <RigidBody colliders={false} position={[0, 0, 0]} type="fixed">
-        <mesh castShadow position={[0, -0.62, 0]} receiveShadow>
-          <boxGeometry args={[7.8, 1.24, 2.25]} />
+        <mesh castShadow position={[0, -0.27, -0.02]} receiveShadow>
+          <boxGeometry args={[5.3, 0.54, 1.56]} />
           <meshStandardMaterial color="#232325" roughness={0.62} />
         </mesh>
-        <mesh position={[0, -0.36, 1.126]} receiveShadow>
-          <planeGeometry args={[7.76, 0.5]} />
+        <mesh position={[0, -0.33, 0.77]} receiveShadow>
+          <planeGeometry args={[5.26, 0.18]} />
           <meshStandardMaterial color="#2a2a2d" roughness={0.7} />
         </mesh>
 
-        <mesh castShadow position={[0, 0.16, -0.56]} receiveShadow>
-          <boxGeometry args={[7.1, 0.46, 0.18]} />
+        <mesh castShadow position={[0, -0.01, -0.41]} receiveShadow>
+          <boxGeometry args={[4.66, 0.18, 0.14]} />
           <meshStandardMaterial color="#2b2c30" roughness={0.58} />
         </mesh>
-        <mesh castShadow position={[0, -0.02, 0.56]} receiveShadow>
-          <boxGeometry args={[7.1, 0.12, 0.16]} />
+        <mesh castShadow position={[0, -0.15, 0.38]} receiveShadow>
+          <boxGeometry args={[4.66, 0.04, 0.1]} />
           <meshStandardMaterial color="#2d2e33" roughness={0.62} />
         </mesh>
-        <mesh castShadow position={[-3.47, 0.08, -0.02]} receiveShadow>
-          <boxGeometry args={[0.14, 0.3, 1.18]} />
+        <mesh castShadow position={[-2.27, -0.07, -0.02]} receiveShadow>
+          <boxGeometry args={[0.08, 0.1, 0.92]} />
           <meshStandardMaterial color="#2a2b2f" roughness={0.6} />
         </mesh>
-        <mesh castShadow position={[3.47, 0.08, -0.02]} receiveShadow>
-          <boxGeometry args={[0.14, 0.3, 1.18]} />
+        <mesh castShadow position={[2.27, -0.07, -0.02]} receiveShadow>
+          <boxGeometry args={[0.08, 0.1, 0.92]} />
           <meshStandardMaterial color="#2a2b2f" roughness={0.6} />
         </mesh>
 
         <mesh
           castShadow
-          position={[0, -0.08, -0.02]}
+          position={[0, -0.07, -0.04]}
           receiveShadow
           rotation={[-0.32, 0, 0]}
         >
-          <boxGeometry args={[7.0, 0.08, 1.04]} />
+          <boxGeometry args={[4.56, 0.04, 0.84]} />
           <meshStandardMaterial color="#16171a" roughness={0.92} />
         </mesh>
 
         {diceDefinitions.map((definition) => (
           <mesh
             key={definition.id}
-            position={[definition.placement.basePosition[0], 0.01, 0.49]}
+            position={[definition.placement.basePosition[0], -0.12, 0.34]}
             receiveShadow
           >
-            <boxGeometry args={[0.94, 0.06, 0.08]} />
+            <boxGeometry args={[0.94, 0.03, 0.05]} />
             <meshStandardMaterial color="#1a1b1d" roughness={0.96} />
           </mesh>
         ))}
 
-        <CuboidCollider args={[3.9, 0.62, 1.125]} position={[0, -0.62, 0]} />
+        <CuboidCollider args={[2.65, 0.27, 0.78]} position={[0, -0.27, -0.02]} />
       </RigidBody>
     </>
   )
